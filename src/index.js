79f43a1fcb6885b0553e3fae91f5d4b16b6eef35
EuGuiXtd <guilhermedpjf@gmail.com>
2022-12-14 17:00:30 -0300

    feat: Requisito 3

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+function geraStringAleatoria(tamanho) {
+  let stringAleatoria = '';
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < tamanho; i += 1) {
+      stringAleatoria += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+  }
+  return stringAleatoria;
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -26,4 +35,12 @@ app.get('/talker/:id', async (req, res) => {
   const talker = await readTalker.getTalkerById(Number(id));
   if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   return res.status(200).json(talker);
+});
+
+app.post('/login', async (req, res) => {
+  const newTalker = { ...req.body };
+  const talker = await readTalker.getAllTalkers();
+  talker.push(newTalker);
+
+  res.status(200).json({ token: geraStringAleatoria(16) });
 });
