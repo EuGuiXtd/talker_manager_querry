f1334210f84e02a4429aa511c22e8201ac30aeb3
EuGuiXtd <guilhermedpjf@gmail.com>
2022-12-14 16:33:08 -0300

    feat: Requisito 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const readTalker = require('./readTalker');
 
 const app = express();
 app.use(express.json());
@@ -13,4 +14,9 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (req, res) => {
+  const talkers = await readTalker.getAllTalkers();
+  return res.status(200).json(talkers);
 });
